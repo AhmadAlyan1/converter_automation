0a52962209eabcd6816b388070a8a2dad6947772
Ahmad Alyan <ahmadalya@edu.hac.ac.il>
2023-01-26 19:51:33 +0200

    Initial commit

---
+++ features/step_definitions/first_steps.rb
@@ -0,0 +1,11 @@
+Given(/^I click on Got it button$/) do
+  puts("Got it button clicked")
+end
+
+  And(/^I see Sq Kilometre in From header$/) do
+  puts("Saw Sq Kilometre in From header")
+end
+
+And(/^I see Sq Metre in To header$/) do
+  puts("Saw Sq Metre in To header")
+end
