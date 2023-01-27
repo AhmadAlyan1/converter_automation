8bbe669630968856665dacc6b80fb591879b2ed0
Ahmad Alyan <ahmadalya@edu.hac.ac.il>
2023-01-27 14:47:41 +0200

    .

--- features/step_definitions/first_steps.rb
+++ features/step_definitions/first_steps.rb
@@ -1,11 +1,23 @@
 Given(/^I click on Got it button$/) do
   puts("Got it button clicked")
+  find_element(id:"android:id/button1").click
 end
 
-  And(/^I see Sq Kilometre in From header$/) do
-  puts("Saw Sq Kilometre in From header")
+And(/^I see "([^"]*)" in From header$/) do |value|
+  actual_value = find_element(id:"header_text_unit_from").text
+  if(actual_value!=value)
+    fail("Expected value is #{value}, but actual value was #{actual_value}!")
+  puts("Got it button clicked")
+  end
 end
 
-And(/^I see Sq Metre in To header$/) do
-  puts("Saw Sq Metre in To header")
+And(/^I see "([^"]*)" in To header$/) do |value|
+  actual_value = find_element(id:"header_text_unit_to").text
+  if(actual_value!=value)
+    fail("Expected value is #{value}, but actual value was #{actual_value}!")
+  end
+end
+
+When(/^I click on Swap button$/) do
+  find_element(id:"fab").click
 end
